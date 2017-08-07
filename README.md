909986ae421caf17dd4f8accc450efb8709828b7
Maximus <maximus@mintdev.local>
2017-08-07 11:29:20 +0300

    v 0.1

---
+++ README.md
@@ -0,0 +1,5 @@
+returns next image from images directory. 
+Call via url like:
+<img src="script-url">
+
+By default images are stored in "images" sub-dir. Or, change path in line #2
---
+++ getimg.php
@@ -0,0 +1,22 @@
+<?php
+const IMG_DIR = __DIR__."/images/";
+$files = glob(IMG_DIR."*.{jpg,png,gif,jpeg}", GLOB_BRACE);
+$next_file = false;
+if (is_file(__DIR__."/next.txt")) {
+	$last_file = file_get_contents(__DIR__."/next.txt");
+	foreach ($files as $key => $file) {
+		if ( strcmp( basename($file), $last_file) == 0 )  {
+			break;
+		}
+	}
+	$next_file = current($files);
+}
+	if (!$next_file) {
+		$next_file = $files[0];
+	}
+
+	header('Content-Type: image/jpeg');
+	//header('image: '.$key);
+	file_put_contents(__DIR__."/next.txt", basename($next_file));
+	readfile($next_file);
+	exit;
