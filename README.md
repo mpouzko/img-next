d9710bcb8f3d9192ff01ff18246b86255626e683
Maximus <maximus@mintdev.local>
2017-08-21 11:18:25 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+v 1.0.
+now we use xmlhttprequest to transfer data from server.
+
+
+Request can contain field [last_file]. In such case next image data is returned. Otherwise, it returns next image data according to internal counter. 
+
+
+Response is in JSON format: 
+base64-coded image [image]
+image name [name]
+
+
+v.0.1
 returns next image from images directory. 
 Call via url like:
 ```
@@ -5,3 +18,4 @@ Call via url like:
 ```
 
 By default images are stored in "images" sub-dir. Or, change path in line #2
+
--- getimg.php
+++ getimg.php
@@ -1,8 +1,17 @@
 <?php
 const IMG_DIR = __DIR__."/images/";
+
 $files = glob(IMG_DIR."*.{jpg,png,gif,jpeg}", GLOB_BRACE);
 $next_file = false;
-if (is_file(__DIR__."/next.txt")) {
+if ($_REQUEST["last_file"]) {
+	foreach ($files as $key => $file) {
+		if ( strcmp( basename($file), $_REQUEST["last_file"]) == 0 )  {
+			break;
+		}
+	}
+	$next_file = current($files);
+}
+elseif (is_file(__DIR__."/next.txt")) {
 	$last_file = file_get_contents(__DIR__."/next.txt");
 	foreach ($files as $key => $file) {
 		if ( strcmp( basename($file), $last_file) == 0 )  {
@@ -11,12 +20,26 @@ if (is_file(__DIR__."/next.txt")) {
 	}
 	$next_file = current($files);
 }
-	if (!$next_file) {
+
+else {
 		$next_file = $files[0];
 	}
 
-	header('Content-Type: image/jpeg');
+	$response = [
+		"image" => base64_encode(file_get_contents($next_file)),
+		"name" => basename($next_file)
+	];
+	header("Access-Control-Allow-Origin: https://render.crowdflower.io");
+	header('Content-Type: application/json');
+	echo json_encode($response);
+if (!$_REQUEST["last_file"]) {
+	file_put_contents(__DIR__."/next.txt", basename($next_file));
+}
+	/*header('Content-Type: image/jpeg');
+
+
 	//header('image: '.$key);
 	file_put_contents(__DIR__."/next.txt", basename($next_file));
 	readfile($next_file);
 	exit;
+	*/
